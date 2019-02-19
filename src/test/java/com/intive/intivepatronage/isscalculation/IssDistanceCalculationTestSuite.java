b1971b2668aae7caa175ce7db7d70661ae2bf910
Grexy <k.greg@wp.pl>
2019-02-19 10:40:32 +0100

    Unit tests correction

--- src/test/java/com/intive/intivepatronage/isscalculation/IssDistanceCalculationTestSuite.java
+++ src/test/java/com/intive/intivepatronage/isscalculation/IssDistanceCalculationTestSuite.java
@@ -22,8 +22,8 @@ public class IssDistanceCalculationTestSuite {
         IssCoordinate issCoordinate1 = new IssCoordinate("-5.7312", "17.9365");
         IssCoordinate issCoordinate2 = new IssCoordinate("-5.4683", "18.2566");
 
-        IssPosition issPosition1 = new IssPosition(issCoordinate1, 1550388225, "success1");
-        IssPosition issPosition2 = new IssPosition(issCoordinate2, 1550388232, "success2");
+        IssPosition issPosition1 = new IssPosition(issCoordinate1, 1550388225L, "success1");
+        IssPosition issPosition2 = new IssPosition(issCoordinate2, 1550388232L, "success2");
 
         List<IssPosition> issPositionsList = new ArrayList<>();
         issPositionsList.add(issPosition1);
@@ -33,14 +33,6 @@ public class IssDistanceCalculationTestSuite {
         double distance = issDistanceCalculation.calculateDistanceBetweenTwoIssPosition(issPositionsList);
 
         //Then
-        assertEquals("-5.7312", issCoordinate1.getLongitude());
-        assertEquals("17.9365", issCoordinate1.getLatitude());
-        assertEquals("-5.4683", issCoordinate2.getLongitude());
-        assertEquals("18.2566", issCoordinate2.getLatitude());
-        assertEquals(1550388225, issPosition1.getTimestamp());
-        assertEquals("success1", issPosition1.getMessage());
-        assertEquals(1550388232, issPosition2.getTimestamp());
-        assertEquals("success2", issPosition2.getMessage());
         assertEquals(2, issPositionsList.size());
         assertEquals(46.02, distance, 0.001);
     }
--- src/test/java/com/intive/intivepatronage/isscalculation/IssSpeedCalculationTestSuite.java
+++ src/test/java/com/intive/intivepatronage/isscalculation/IssSpeedCalculationTestSuite.java
@@ -23,9 +23,9 @@ public class IssSpeedCalculationTestSuite {
         IssCoordinate issCoordinate2 = new IssCoordinate("32.2263", "51.2158");
         IssCoordinate issCoordinate3 = new IssCoordinate("32.8687", "51.2695");
 
-        IssPosition issPosition1 = new IssPosition(issCoordinate1, 1550394715, "success1");
-        IssPosition issPosition2 = new IssPosition(issCoordinate2, 1550394721, "success2");
-        IssPosition issPosition3 = new IssPosition(issCoordinate3, 1550394728, "success3");
+        IssPosition issPosition1 = new IssPosition(issCoordinate1, 1550394715L, "success1");
+        IssPosition issPosition2 = new IssPosition(issCoordinate2, 1550394721L, "success2");
+        IssPosition issPosition3 = new IssPosition(issCoordinate3, 1550394728L, "success3");
 
         List<IssPosition> issPositionsList = new ArrayList<>();
         issPositionsList.add(issPosition1);
@@ -36,18 +36,6 @@ public class IssSpeedCalculationTestSuite {
         double speed = issSpeedCalculation.calculateIssSpeed(issPositionsList);
 
         //Then
-        assertEquals("31.5855", issCoordinate1.getLongitude());
-        assertEquals("51.1582", issCoordinate1.getLatitude());
-        assertEquals("32.2263", issCoordinate2.getLongitude());
-        assertEquals("51.2158", issCoordinate2.getLatitude());
-        assertEquals("32.8687", issCoordinate3.getLongitude());
-        assertEquals("51.2695", issCoordinate3.getLatitude());
-        assertEquals(1550394715, issPosition1.getTimestamp());
-        assertEquals("success1", issPosition1.getMessage());
-        assertEquals(1550394721, issPosition2.getTimestamp());
-        assertEquals("success2", issPosition2.getMessage());
-        assertEquals(1550394728, issPosition3.getTimestamp());
-        assertEquals("success3", issPosition3.getMessage());
         assertEquals(3, issPositionsList.size());
         assertEquals(11.01, speed, 0.001);
     }
